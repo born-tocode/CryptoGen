47450a76418ef4c9748936f8a91502a18cdf215e
Mateusz Boguszewski <mateo.b@data.pl>
2019-10-30 13:11:07 +0100

    Initial commit

---
+++ RSAkeyGen/README.md
@@ -0,0 +1,4 @@
+#RSAkeyGen
+This key generator generates 1024 - 16384 bits pair keys in RSA algorithm. 
+Just run and choose one of five numbers of bits. You can choose - printing Keys only in console(copy - paste) 
+or - only to files. Nice exiting from program by 'Q'. Both keys are may be opened from Notepad or another text editor.
---
+++ RSAkeyGen/src/com/borntocode/RSAkeyGen.java
@@ -0,0 +1,158 @@
+package com.borntocode;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.*;
+import java.util.*;
+
+class RSAkeyGen {
+
+    private final Base64.Encoder ENCODER = Base64.getEncoder();
+    private final SortedMap<Integer, Integer> KEYSIZE = new TreeMap<>();
+    private final Scanner IN = new Scanner(System.in);
+    private final PrintWriter OUT = new PrintWriter(System.out, true);
+    private final Path FILE_PRV = Paths.get("privateKey.key");
+    private final Path FILE_PUB = Paths.get("publicKey.pub");
+    private final String[] TXT = {
+            "\n-----BEGIN RSA PRIVATE KEY-----\n", "-----BEGIN RSA PUBLIC KEY-----\n",
+            "PRIVATE KEY FORMAT", "PRIVATE KEY ALGORITHM", "\n-----END RSA PRIVATE KEY-----",
+            "\n-----END RSA PUBLIC KEY-----", "PUBLIC KEY FORMAT", "PUBLIC KEY ALGORITHM"
+    };
+    private KeyPairGenerator kpg;
+    private KeyPair kp;
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    public static void main(String[] args) {
+        RSAkeyGen kg = new RSAkeyGen();
+        kg.putValues();
+        kg.printMsg();
+        try {
+            kg.generator();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        kg.infoAboutKeys();
+        kg.getAnswer();
+    }
+
+    private void putValues() {
+        KEYSIZE.put(0, 1024);
+        KEYSIZE.put(1, 2048);
+        KEYSIZE.put(2, 4096);
+        KEYSIZE.put(3, 8192);
+        KEYSIZE.put(4, 12288);
+        KEYSIZE.put(5, 16384);
+    }
+
+    private void printMsg() {
+        OUT.println();
+        OUT.println("Please type a digit for strength of RSA:");
+        OUT.println();
+
+        for (Integer k : KEYSIZE.keySet()) {
+            Integer v = KEYSIZE.get(k);
+            OUT.print(k + ". " + v + " / ");
+        }
+
+        OUT.println();
+    }
+
+    private int getKey() {
+        int key;
+        IN.hasNextInt();
+        key = IN.nextInt();
+        IN.nextLine();
+        return key;
+    }
+
+    private void getAnswer() {
+        String ans = null;
+
+        OUT.println();
+        OUT.println(
+                "Do you want print buffer in this console(buffer are cleared " +
+                "after end of program)? [ Y/N ]\nIf 'N' keys will automatically " +
+                "saved to files without printing in this console.");
+        OUT.println();
+        OUT.println("'Q' terminate process.");
+
+        try {
+            while (ans == null) {
+                ans = IN.findInLine("[ynqYNQ]");
+                IN.nextLine();
+                if (ans.equalsIgnoreCase("Y")) {
+                    printKeys();
+                } else if (ans.equalsIgnoreCase("N")) {
+                    saveKeysToFiles();
+                } else if (ans.equalsIgnoreCase("Q")) {
+                    IN.close();
+                    OUT.close();
+                    System.exit(0);
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.err.println("Bad choice. Try again.. or quit 'Q'");
+        } finally {
+            getAnswer();
+        }
+    }
+
+    private void generator() throws NoSuchAlgorithmException {
+        kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(KEYSIZE.get(getKey()));
+
+        kp = kpg.generateKeyPair();
+        privateKey = kp.getPrivate();
+        publicKey = kp.getPublic();
+    }
+
+    private void infoAboutKeys() {
+        OUT.println();
+        OUT.println(TXT[2].toLowerCase() + ": " + privateKey.getFormat());
+        OUT.println(TXT[3].toLowerCase() + ": " + privateKey.getAlgorithm());
+        OUT.println();
+        OUT.println(TXT[6].toLowerCase() + ": " + publicKey.getFormat());
+        OUT.println(TXT[7].toLowerCase() + ": " + publicKey.getAlgorithm());
+    }
+
+    private void printKeys() {
+        OUT.print(TXT[0]);
+        OUT.print(ENCODER.encodeToString(privateKey.getEncoded()));
+        OUT.print(TXT[4]);
+
+        OUT.println();
+        OUT.println();
+
+        OUT.print(TXT[1]);
+        OUT.print(ENCODER.encodeToString(publicKey.getEncoded()));
+        OUT.print(TXT[5]);
+        OUT.println();
+    }
+
+    private void saveKeysToFiles() {
+
+        try (FileOutputStream fOutPrv = new FileOutputStream(FILE_PRV.toFile());
+             FileOutputStream fOutPub = new FileOutputStream(FILE_PUB.toFile())) {
+
+            fOutPrv.write(TXT[0].getBytes());
+            fOutPrv.write(ENCODER.encode(privateKey.getEncoded()));
+            fOutPrv.write(TXT[4].getBytes());
+
+            OUT.println();
+            OUT.println();
+
+            fOutPub.write(TXT[1].getBytes());
+            fOutPub.write(ENCODER.encode(publicKey.getEncoded()));
+            fOutPub.write(TXT[5].getBytes());
+
+            OUT.println("Keys are saved in program directory.");
+            OUT.println();
+            OUT.println();
+        } catch (IOException e) {
+            OUT.println("Input/output error");
+            OUT.println(e.getMessage());
+        }
+    }
+}
