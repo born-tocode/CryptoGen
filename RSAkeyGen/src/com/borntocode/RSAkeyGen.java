6e67c3a6d0a5982e0f4b2d6fa1a74310539eeb61
Mateusz Boguszewski <mateo.b@data.pl>
2019-11-07 20:50:51 +0100

    [Scope test] before

--- RSAkeyGen/src/com/borntocode/RSAkeyGen.java
+++ RSAkeyGen/src/com/borntocode/RSAkeyGen.java
@@ -1,12 +1,14 @@
 package com.borntocode;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 class RSAkeyGen {
 
-    //todo: too many instances of Generator > OutBuffer doesn't work properly
     private final Generator generator = new Generator();
     private final OutBuffer outBuffer = new OutBuffer();
     private final PrintWriter out = new PrintWriter(System.out, true);
@@ -15,7 +17,7 @@ class RSAkeyGen {
     private final List<Integer> listSizesOfKeys = List.of(1024, 2048, 4096, 8192, 12288, 16384);
     private final ListIterator<Integer> listIterator = listSizesOfKeys.listIterator();
     private final Locale currentLocale = new Locale("en", "US");
-    private final ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", currentLocale);
+    private final File FILENAME = new File("/src/MessagesBundle");
 
 
     public static void main(String[] args) {
@@ -33,100 +35,124 @@ class RSAkeyGen {
     private void firstDialog() {
         int count = 0;
         int digFromUser;
+        try (FileInputStream fin = new FileInputStream(FILENAME.toString())) {
+            ResourceBundle messages = ResourceBundle.getBundle(fin.toString(), currentLocale);
 
-        out.println();
-        out.println(messages.getString("dialog.please.type.a.digit"));
-        out.println();
+            out.println();
+            out.println(messages.getString("dialog.please.type.a.digit"));
+            out.println();
 
-        while (listIterator.hasNext()) {
-            int sizeOfKeys = listIterator.next();
-            out.print(count + ". " + sizeOfKeys + " / ");
-            count++;
-        }
-
-        try {
-            in.hasNextInt();
-            digFromUser = in.nextInt();
-            in.nextLine();
-            switch (digFromUser) {
-                case 0:
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                    generator.generateKeys(listSizesOfKeys.get(digFromUser));
-                    generator.extractKeys();
-                    break;
-                default:
-                    out.println(messages.getString("dialog.bad.digit") + digFromUser);
+            while (listIterator.hasNext()) {
+                int sizeOfKeys = listIterator.next();
+                out.print(count + ". " + sizeOfKeys + " / ");
+                count++;
             }
-        } catch (InputMismatchException | NoSuchAlgorithmException e) {
-            out.println(messages.getString("dialog.incorrect.choice"));
+
+            try {
+                in.hasNextInt();
+                digFromUser = in.nextInt();
+                in.nextLine();
+                switch (digFromUser) {
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5:
+                        generator.generateKeys(listSizesOfKeys.get(digFromUser));
+                        generator.extractKeys();
+                        break;
+                    default:
+                        out.println(messages.getString("dialog.bad.digit") + digFromUser);
+                }
+            } catch (InputMismatchException | NoSuchAlgorithmException e) {
+                out.println("dialog.incorrect.choice");
+            }
+        } catch (IOException e) {
+            out.println("dialog.properties.file.not.found");
         }
     }
 
     private void secondDialog() {
         String strFromUser;
 
-        out.println();
-        out.println(messages.getString(messages.getString("dialog.do.you.want.print")));
-        out.println();
-        out.println(messages.getString("dialog.q.terminate.process"));
+        try (FileInputStream fin = new FileInputStream(FILENAME.toString())) {
+            ResourceBundle messages = ResourceBundle.getBundle(FILENAME.toString(), currentLocale);
+            out.println();
+            out.println(messages.getString("dialog.do.you.want.print"));
+            out.println();
+            out.println(messages.getString("dialog.q.terminate.process"));
 
-        try {
-            strFromUser = in.findInLine("[ynqYNQ]");
-            in.nextLine();
+            try {
+                strFromUser = in.findInLine("[ynqYNQ]");
+                in.nextLine();
 
-            switch (strFromUser.toUpperCase()) {
-                case "Y":
-                    printKeysToConsole();
-                    break;
-                case "N":
-                    outBuffer.saveKeysToFiles();
-                    break;
-                case "Q":
-                    in.close();
-                    out.close();
-                    System.exit(0);
-                    break;
-                default:
-                    out.println("Your " + strFromUser);
+                switch (strFromUser.toUpperCase()) {
+                    case "Y":
+                        printKeysToConsole();
+                        break;
+                    case "N":
+                        outBuffer.saveKeysToFiles();
+                        break;
+                    case "Q":
+                        in.close();
+                        out.close();
+                        System.exit(0);
+                        break;
+                    default:
+                        out.println("Your " + strFromUser);
+                }
+            } catch (InputMismatchException e) {
+                out.println(messages.getString("dialog.bad.choice.try.again.or.quit.q"));
             }
-        } catch (InputMismatchException e) {
-            System.err.println(messages.getString("dialog.bad.choice.try.again.or.quit.q"));
+        } catch (IOException e) {
+            out.println("dialog.properties.file.not.found");
         }
     }
 
     private void displayInfoAboutKeys() {
-        out.println();
-        out.println(messages.getString("info.private.key.format").toLowerCase() + ": "
-                + generator.getPrivateKey().getFormat());
-        out.println(messages.getString("info.private.key.algorithm").toLowerCase() + ": "
-                + generator.getPrivateKey().getAlgorithm());
-        out.println();
-        out.println(messages.getString("info.public.key.format").toLowerCase() + ": "
-                + generator.getPublicKey().getFormat());
-        out.println(messages.getString("info.public.key.algorithm").toLowerCase() + ": "
-                + generator.getPublicKey().getAlgorithm());
-        out.print(messages.getString("dialog.separator"));
+        try (FileInputStream fin = new FileInputStream(FILENAME.toString())) {
+            ResourceBundle messages = ResourceBundle.getBundle(FILENAME.toString(), currentLocale);
+            out.println();
+            out.println(messages.getString("info.private.key.format").toLowerCase() + ": "
+                    + generator.getPrivateKey().getFormat());
+            out.println(messages.getString("info.private.key.algorithm").toLowerCase() + ": "
+                    + generator.getPrivateKey().getAlgorithm());
+            out.println();
+            out.println(messages.getString("info.public.key.format").toLowerCase() + ": "
+                    + generator.getPublicKey().getFormat());
+            out.println(messages.getString("info.public.key.algorithm").toLowerCase() + ": "
+                    + generator.getPublicKey().getAlgorithm());
+            out.print(messages.getString("dialog.separator"));
+        }
+//        catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+        catch (IOException e) {
+            out.println("dialog.properties.file.not.found");
+        }
     }
 
     private void printKeysToConsole() {
-        out.println(messages.getString("dialog.separator"));
-        out.println();
-        out.println(messages.getString(messages.getString("key.begin.rsa.private.key")));
-        out.println(encoder.encodeToString(generator.getPrivateKey().getEncoded()));
-        out.println(messages.getString(messages.getString("key.end.rsa.private.key")));
+        try (FileInputStream fin = new FileInputStream(FILENAME.toString())) {
+            ResourceBundle messages = ResourceBundle.getBundle(FILENAME.toString(), currentLocale);
+            out.println(messages.getString("dialog.separator"));
+            out.println();
+            out.println(messages.getString(messages.getString("key.begin.rsa.private.key")));
+            out.println(encoder.encodeToString(generator.getPrivateKey().getEncoded()));
+            out.println(messages.getString(messages.getString("key.end.rsa.private.key")));
 
-        out.println();
-        out.println();
+            out.println();
+            out.println();
 
-        out.println(messages.getString(messages.getString("key.begin.rsa.public.key")));
-        out.println(encoder.encodeToString(generator.getPublicKey().getEncoded()));
-        out.println(messages.getString(messages.getString("key.end.rsa.public.key")));
-        out.println();
-        out.print(messages.getString("dialog.separator"));
+            out.println(messages.getString(messages.getString("key.begin.rsa.public.key")));
+            out.println(encoder.encodeToString(generator.getPublicKey().getEncoded()));
+            out.println(messages.getString(messages.getString("key.end.rsa.public.key")));
+            out.println();
+            out.print(messages.getString("dialog.separator"));
+        } catch (IOException e) {
+            out.println("dialog.properties.file.not.found");
+        }
     }
 
     private void closeIO() {
