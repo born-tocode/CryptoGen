dc6c28e64a07b5c7b9ad713c7d013411ebf50546
Mateusz Boguszewski <mateo.b@data.pl>
2019-11-09 20:07:13 +0100

    [Refactoring] smarter buildViewOfKeysToConsole()

--- RSAkeyGen/src/com/borntocode/FlowControl.java
+++ RSAkeyGen/src/com/borntocode/FlowControl.java
@@ -1,26 +1,29 @@
 package com.borntocode;
 
+import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.ByteBuffer;
+import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 class FlowControl {
-
-    //todo: local variables(var), return result to local variables, KeysProcessor with hardcoded texts
 
     private final PrintStream out = new PrintStream(System.out);
     private final Scanner in = new Scanner(System.in);
     private final Locale currentLocale = new Locale("en", "US");
     private final ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", currentLocale);
     private List<ByteBuffer> keysBuffer = new ArrayList<>();
+    private boolean flag = true;
 
 
     void startMainLoop() {
-        firstDialog();
-//        displayInfoAboutKeys();
-        secondDialog();
-        closeIO();
+//        do {
+            firstDialog();
+            secondDialog();
+//        } while (flag);
+
+        closeIO(in, out);
     }
 
     private void firstDialog() {
@@ -63,10 +66,10 @@ class FlowControl {
         } catch (InputMismatchException | NoSuchAlgorithmException e) {
             out.println(messages.getString("dialog.incorrect.choice"));
         }
-
     }
 
     private void secondDialog() {
+        final String[] prvPub = {"private", "public"};
         var strFromUser = "";
 
         out.println();
@@ -80,16 +83,15 @@ class FlowControl {
 
             switch (strFromUser.toUpperCase()) {
                 case "Y":
-                    buildViewOfKeysToConsole(keysBuffer.get(0), keysBuffer.get(1));
+                    buildViewOfKeysToConsole(keysBuffer, prvPub);
                     break;
                 case "N":
                     new OutBuffer().saveKeysToFiles(keysBuffer.get(0), keysBuffer.get(1));
                     break;
                 case "Q":
-                    in.close();
-                    out.close();
+                    flag = false;
+                    closeIO(in, out);
                     System.exit(0);
-                    break;
                 default:
                     out.println("Your " + strFromUser);
             }
@@ -98,38 +100,43 @@ class FlowControl {
         }
     }
 
-//    private void displayInfoAboutKeys() {
-//        out.println();
-//        out.println(messages.getString("info.private.key.format").toLowerCase() + ": "
-//                + generator.getPrivateKey().getFormat());
-//        out.println(messages.getString("info.private.key.algorithm").toLowerCase() + ": "
-//                + generator.getPrivateKey().getAlgorithm());
-//        out.println();
-//        out.println(messages.getString("info.public.key.format").toLowerCase() + ": "
-//                + generator.getPublicKey().getFormat());
-//        out.println(messages.getString("info.public.key.algorithm").toLowerCase() + ": "
-//                + generator.getPublicKey().getAlgorithm());
-//        out.print(messages.getString("dialog.separator"));
-//    }
-
-    private void buildViewOfKeysToConsole(ByteBuffer privateKey, ByteBuffer publicKey) {
-        out.println(messages.getString("dialog.separator"));
+    private void displayInfoAboutKeys(KeyPair keys) {
         out.println();
-        out.println(messages.getString("key.begin.rsa.private.key"));
-        out.println(privateKey.get());
-        out.println(messages.getString("key.end.rsa.private.key"));
-
+        out.println(messages.getString("info.private.key.format").toLowerCase() + ": "
+                + keys.getPrivate().getFormat());
+        out.println(messages.getString("info.private.key.algorithm").toLowerCase() + ": "
+                + keys.getPrivate().getAlgorithm());
         out.println();
-        out.println();
-
-        out.println(messages.getString("key.begin.rsa.public.key"));
-        out.println(publicKey.get());
-        out.println(messages.getString("key.end.rsa.public.key"));
-        out.println();
+        out.println(messages.getString("info.public.key.format").toLowerCase() + ": "
+                + keys.getPublic().getFormat());
+        out.println(messages.getString("info.public.key.algorithm").toLowerCase() + ": "
+                + keys.getPublic().getAlgorithm());
         out.print(messages.getString("dialog.separator"));
     }
 
-    private void closeIO() {
+    private void buildViewOfKeysToConsole(List<ByteBuffer> keysBuffer, String[] prvPub) {
+        var index = 0;
+
+        try {
+            for (ByteBuffer key : keysBuffer) {
+                if (index == 0) out.println(messages.getString("dialog.separator"));
+                out.write('\n');
+                out.println(messages.getString("key.begin.rsa." + prvPub[index] + ".key"));
+                out.write(key.array());
+                out.write('\n');
+                out.println(messages.getString("key.end.rsa." + prvPub[index] + ".key"));
+
+                index++;
+
+                out.write('\n');
+                out.write('\n');
+            }
+        } catch (IOException e) {
+            out.println("I/O exception");
+        }
+    }
+
+    private void closeIO(Scanner in, PrintStream out) {
         in.close();
         out.close();
     }
